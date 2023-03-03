3a70c2e0af57927a1c63a5633587419ec02de5c5
Philipp Braun <102253068+pbraunQA@users.noreply.github.com>
2023-03-03 12:04:43 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,5 @@
-'from * as http from "http";'
 import * as http from 'http';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
 const server = http.createServer((req, res) => {
   res.end('Hello, World!');
 });
@@ -11,4 +7,5 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
 
